ef54f6dcf9709229afd6f431aad286de683bd8d0
chirag <chiragtharad01@gmail.com>
2026-06-16 14:53:15 +0530

    Added validations to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ApplicationRecord
-    title :
+    MAX_TITLE_LENGTH = 125
+    MAX_DESCRIPTION_LENGTH = 10000
+    VALID_TITLE_REGEX = /\A.*[a-zA-Z0-9].*\z/i
+
+    validates :title, presence: true, length: { maximum: MAX_TITLE_LENGTH }, format: { with: VALID_TITLE_REGEX }
+    validates :description, presence: true, length: { maximum: MAX_DESCRIPTION_LENGTH }
+    validates_inclusion_of :is_bloggable, in: [true, false]
 end
